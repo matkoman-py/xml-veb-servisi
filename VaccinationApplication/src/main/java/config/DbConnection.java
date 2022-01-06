470ccf1dded1245acca43b3d5269da0f89ae2956
matkoman-py <62108859+matkoman-py@users.noreply.github.com>
2022-01-06 19:51:08 +0100

    small bugfix

--- VaccinationApplication/src/main/java/config/DbConnection.java
+++ VaccinationApplication/src/main/java/config/DbConnection.java
@@ -2,7 +2,10 @@ package config;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,6 +13,8 @@ import org.springframework.context.annotation.Configuration;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Getter
+@Setter
 public class DbConnection {
 
     @Value("${db.username}")
@@ -23,4 +28,36 @@ public class DbConnection {
 
     @Value("${db.driver}")
     private String dbDriver;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getDbUrl() {
+		return dbUrl;
+	}
+
+	public void setDbUrl(String dbUrl) {
+		this.dbUrl = dbUrl;
+	}
+
+	public String getDbDriver() {
+		return dbDriver;
+	}
+
+	public void setDbDriver(String dbDriver) {
+		this.dbDriver = dbDriver;
+	}
 }
--- VaccinationApplication/src/main/java/dao/InteresovanjeDAO.java
+++ VaccinationApplication/src/main/java/dao/InteresovanjeDAO.java
@@ -1,4 +1,23 @@
 package dao;
 
+import org.springframework.stereotype.Component;
+
+import lombok.RequiredArgsConstructor;
+import model.documents.Interesovanje;
+
+@Component
+@RequiredArgsConstructor
 public class InteresovanjeDAO {
+	
+	private final String collectionId = "/db/vaccination-application/interesovanja";
+    private final DaoUtils daoUtils;
+    
+    
+    public String save() {
+        String documentId =  interesovanje.getPodaciOPrimaocu().getJMBG()+ ".xml";
+        Interesovanje interesovanje = new Interesovanje();
+        daoUtils.createResource(collectionId, interesovanje, documentId, Interesovanje.class);
+        return interesovanje.getPodaciOPrimaocu().getJMBG();
+    }
 }
+
