94a045dd0815ed6cce4d6cdb867fe6c4e06d3627
marko16 <marko.suljak80@gmail.com>
2022-01-06 21:58:02 +0100

    minor bugfix

---
+++ OfficialsApplication/src/test/java/com/example/officialsapplication/OfficialsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.officialsapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfficialsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
--- VaccinationApplication/src/main/java/dao/InteresovanjeDAO.java
+++ VaccinationApplication/src/main/java/dao/InteresovanjeDAO.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Component;
 import lombok.RequiredArgsConstructor;
 import model.documents.Interesovanje;
 
+import java.util.Optional;
+
 @Component
 @RequiredArgsConstructor
 public class InteresovanjeDAO {
@@ -13,11 +15,21 @@ public class InteresovanjeDAO {
     private final DaoUtils daoUtils;
     
     
-    public String save() {
-        String documentId =  interesovanje.getPodaciOPrimaocu().getJMBG()+ ".xml";
-        Interesovanje interesovanje = new Interesovanje();
-        daoUtils.createResource(collectionId, interesovanje, documentId, Interesovanje.class);
-        return interesovanje.getPodaciOPrimaocu().getJMBG();
+//    public String save() {
+//        String documentId = interesovanje.getPodaciOPrimaocu().getJMBG()+ ".xml";
+//        Interesovanje interesovanje = new Interesovanje();
+//        daoUtils.createResource(collectionId, interesovanje, documentId, Interesovanje.class);
+//        return interesovanje.getPodaciOPrimaocu().getJMBG();
+//    }
+
+    public String get(String id) {
+        String resourceContent = daoUtils.getResource(collectionId, id);
+//        if (resourceContent.equals(""))
+//            return Optional.empty();
+//        Interesovanje interesovanje = mapper.convertToObject(resourceContent);
+//        if (interesovanje == null)
+//            return Optional.empty();
+        return resourceContent;
     }
 }
 
