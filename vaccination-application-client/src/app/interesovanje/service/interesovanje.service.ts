c02985f8582ac641f2f81eafe313c9cfe77d8915
matkoman-py <62108859+matkoman-py@users.noreply.github.com>
2022-02-18 23:48:19 +0100

    small fix for rdf extraction of interesovanje

--- vaccination-application-client/src/app/interesovanje/service/interesovanje.service.ts
+++ vaccination-application-client/src/app/interesovanje/service/interesovanje.service.ts
@@ -23,7 +23,8 @@ export class InteresovanjeService {
       dayString = '0' + day.toString();
     }
     var date = today.getFullYear() + '-' + monthString + '-' + dayString;
-    var xml = `<int:interesovanje about="http://www.ftn.uns.ac.rs/interesovanje/${interesovanje.jmbg}" rel="pred:answeredBy" href="string" xmlns:int="http://www.ftn.uns.ac.rs/interesovanje">
+    var xml = `<int:interesovanje about="http://www.ftn.uns.ac.rs/interesovanje/${interesovanje.jmbg}" xmlns:pred="http://www.ftn.uns.ac.rs/predicate/" 
+    rel="pred:answeredBy" href="string" xmlns:int="http://www.ftn.uns.ac.rs/interesovanje">
   <int:Podaci_o_primaocu>
     <int:Drzavljanstvo>${interesovanje.drzavljanstvo}</int:Drzavljanstvo>
     <int:JMBG property="pred:jmbg" datatype="xs:string">${interesovanje.jmbg}</int:JMBG>
